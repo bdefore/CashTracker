f624b6fb9b86fd93b23639b05cd2335106546d68
Buck DeFore <bdefore@gmail.com>
2011-12-20 12:37:14 +0100

    prep for exposing db.coffee for data model rather than redundancies in template views

--- src/coffee/db.coffee
+++ src/coffee/db.coffee
@@ -5,15 +5,13 @@ module.exports = class DB
 
   # Mongodb schema
 
-  Schema = mongoose.Schema
-
-  BillSchema = new Schema {
+  @Bill = mongoose.model 'Bill' , new mongoose.Schema {
     serial        : String,
     denomination  : Number,
     currency      : String
   }
 
-  SightingSchema = new Schema {
+  @Sighting = mongoose.model 'Sighting', new mongoose.Schema {
     serial        : String,
     latitude      : Number,
     longitude     : Number,
@@ -21,60 +19,54 @@ module.exports = class DB
     submitterId   : String
   }
 
-  mongoose.model 'Bill', BillSchema
-  Bill = mongoose.model 'Bill'
-
-  mongoose.model 'Sighting', SightingSchema
-  Sighting = mongoose.model 'Sighting'
-
-  @connect: () ->
+  @connect: () =>
     mongoose.connect conf.database
     console.log "MongoDB connection success..."
 
-  @prepopulate: () ->
+  @prepopulate: () =>
 
     console.log "Checking for existing data..."
 
-    Sighting.findOne null, (error, result) ->
+    @Sighting.findOne null, (error, result) =>
       if result
         console.log "Found a sighting... skipping dummy data creation..."
       else
         console.log "No sightings found, filling with dummy data..."
 
-        b = new Bill
+        b = new @Bill
           serial: 'X18084287225'
           denomination: 20
           currency: 'Euro'
         b.save()
 
-        b = new Bill
+        b = new @Bill
           serial: 'Y81450250492'
           denomination: 10
           currency: 'Euro'
         b.save()
 
-        s = new Sighting
+        s = new @Sighting
           serial: 'X18084287225'
           latitude: "41.377301033335414"
           longitude: "2.189307280815329"
           comment: 'I got this from my mother'
         s.save()
 
-        s = new Sighting
+        s = new @Sighting
           serial: 'Y81450250492'
           latitude: "31.377301033335414"
           longitude: "-32.189307280815329"
           comment: 'I got this from a restaurant'
         s.save()
 
-        s = new Sighting
+        s = new @Sighting
           serial: 'Y81450250492'
           latitude: "61.377301033335414"
           longitude: "-12.189307280815329"
           comment: 'I got this from my sister'
         s.save()
 
-        s = new Sighting
+        s = new @Sighting
           serial: 'Y81450250492'
           latitude: "11.377301033335414"
           longitude: "-22.189307280815329"
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
   var DB;
 
   module.exports = DB = (function() {
-    var Bill, BillSchema, Schema, Sighting, SightingSchema, conf, mongoose;
+    var conf, mongoose;
 
     function DB() {}
 
@@ -10,29 +10,19 @@
 
     conf = require('./conf');
 
-    Schema = mongoose.Schema;
-
-    BillSchema = new Schema({
+    DB.Bill = mongoose.model('Bill', new mongoose.Schema({
       serial: String,
       denomination: Number,
       currency: String
-    });
+    }));
 
-    SightingSchema = new Schema({
+    DB.Sighting = mongoose.model('Sighting', new mongoose.Schema({
       serial: String,
       latitude: Number,
       longitude: Number,
       comment: String,
       submitterId: String
-    });
-
-    mongoose.model('Bill', BillSchema);
-
-    Bill = mongoose.model('Bill');
-
-    mongoose.model('Sighting', SightingSchema);
-
-    Sighting = mongoose.model('Sighting');
+    }));
 
     DB.connect = function() {
       mongoose.connect(conf.database);
@@ -41,46 +31,46 @@
 
     DB.prepopulate = function() {
       console.log("Checking for existing data...");
-      return Sighting.findOne(null, function(error, result) {
+      return DB.Sighting.findOne(null, function(error, result) {
         var b, s;
         if (result) {
           return console.log("Found a sighting... skipping dummy data creation...");
         } else {
           console.log("No sightings found, filling with dummy data...");
-          b = new Bill({
+          b = new DB.Bill({
             serial: 'X18084287225',
             denomination: 20,
             currency: 'Euro'
           });
           b.save();
-          b = new Bill({
+          b = new DB.Bill({
             serial: 'Y81450250492',
             denomination: 10,
             currency: 'Euro'
           });
           b.save();
-          s = new Sighting({
+          s = new DB.Sighting({
             serial: 'X18084287225',
             latitude: "41.377301033335414",
             longitude: "2.189307280815329",
             comment: 'I got this from my mother'
           });
           s.save();
-          s = new Sighting({
+          s = new DB.Sighting({
             serial: 'Y81450250492',
             latitude: "31.377301033335414",
             longitude: "-32.189307280815329",
             comment: 'I got this from a restaurant'
           });
           s.save();
-          s = new Sighting({
+          s = new DB.Sighting({
             serial: 'Y81450250492',
             latitude: "61.377301033335414",
             longitude: "-12.189307280815329",
             comment: 'I got this from my sister'
           });
           s.save();
-          s = new Sighting({
+          s = new DB.Sighting({
             serial: 'Y81450250492',
             latitude: "11.377301033335414",
             longitude: "-22.189307280815329",
@@ -94,6 +84,6 @@
 
     return DB;
 
-  })();
+  }).call(this);
 
 }).call(this);
